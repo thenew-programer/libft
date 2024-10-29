6d7441b63ac768ec0153ce1ccdadca83598827f8
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-10-29 08:52:53 +0100

    libft - strnstr: done

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -17,17 +17,18 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	j;
 
-	if (!needle)
+	if (!needle || !*needle)
 		return ((char *)haystack);
 	i = 0;
 	while (i < len && haystack[i])
 	{
 		j = 0;
-		while (haystack[i + j] == needle[j] && haystack[i])
+		if (needle[j] == haystack[i])
 		{
-			if (!needle[j + 1])
+			while (haystack[i + j] == needle[j] && (i + j) < len)
+				j++;
+			if (!needle[j])
 				return ((char *)haystack + i);
-			j++;
 		}
 		i++;
 	}
